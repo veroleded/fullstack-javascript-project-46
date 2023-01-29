dbab7b5dace9d5affe01b44387afd584134d42ca
Timur Kharisov <timur1776@gmail.com>
2023-01-29 16:35:27 +0300

    add format in gendiff, add index.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,6 @@ import { program } from "commander";
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
-
-program.parse();
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format')
+program.parse(process.argv);

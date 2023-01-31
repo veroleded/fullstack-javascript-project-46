968b479e565789629cfe03b09e4f775f57ca1cc2
Timur Kharisov <timur1776@gmail.com>
2023-01-31 22:12:01 +0300

    ckeck gitHub actions

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,6 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
 const genFileData = (fileName) => readFileSync(getFixturePath(fileName), 'utf-8');
+
 test('1', () => {
   const expected = genFileData('expected.txt');
   const actual = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
